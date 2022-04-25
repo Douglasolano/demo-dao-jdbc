f014a2adba2e4ced83d68ad11c5deb9d46bf95fe
Douglas Moraes <douglasmoraessolano@gmail.com>
2022-04-24 22:51:58 -0300

    Classe entidade vendedor

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Programa {
 
@@ -8,6 +11,9 @@ public class Programa {
 		
 		Departamento obj = new Departamento(1, "Books");
 		System.out.println(obj);
+		
+		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(vendedor);
 	}
 
 }
