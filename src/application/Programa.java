4cc83e59468c69f22d6b64010e8cf6103ee99147
Douglas Moraes <douglasmoraessolano@gmail.com>
2022-04-24 23:48:10 -0300

    Implementacao findById

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,23 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.FabricaDao;
 import model.dao.VendedorDao;
-import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Departamento obj = new Departamento(1, "Books");
-		System.out.println(obj);
-		
-		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(vendedor);
-		
 		VendedorDao vendedorDao = FabricaDao.createVendedorDao();
+		
+		Vendedor vendedor = vendedorDao.findById(3);
+		System.out.println(vendedor);
 	}
 
 }
